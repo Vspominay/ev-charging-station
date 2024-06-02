92a1be6bee9baf6a9aa1981b1574cd2d6bccfda4
devDmytroHorkun <d.gorkun@itvsystems.com.ua>
2024-06-02 16:57:58 +0300

    feature(theme): update product assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { APP_NAME } from '@core/constants/app-name.constant';
 import { hasToken } from '@features/auth/data-access/guards/has-token-access.guard';
 
 export const routes: Routes = [
@@ -9,11 +10,13 @@ export const routes: Routes = [
   },
   {
     path: 'auth',
+    title: `Welcome to ${APP_NAME}`,
     loadChildren: () => import('./features/auth/auth.routes').then(r => r.ROUTES)
   },
   {
     path: '',
     canActivate: [hasToken],
+    title: APP_NAME,
     loadComponent: () => import('./features/features-root.component').then(m => m.FeaturesRootComponent),
     children: [
       {
